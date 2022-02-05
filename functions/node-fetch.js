3a3c5cd52b2b85e96db2c1f05dfe0dd24812d750
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-02-04 22:01:31 -0600

    removed sensitive info

--- functions/node-fetch.js
+++ functions/node-fetch.js
@@ -1,13 +1,12 @@
-const axios = require('/node_modules/axios');
+const axios = require("axios");
 
 exports.handler = function instagram(_, _, callback) {
-const token = process.env.INSTAGRAM_ACCESS_TOKEN;
-  // const token = 'IGQVJXZATJabHVzSWlYRTJpektYVWpaelVPdWIxRDJ2OUFkeDNNSmdIOEpXUURCbkhWYWIzX1d0YkkzanlLeVNIY0RjeXRiLUFNR2ktQnRnVGhNU252Ql9IRkZAGQnV1RzFUMXNla2M1QWhWLVBtS1FvdwZDZD'
-const userId = 4226862094085798;
-const endpoint = 'https://graph.instagram.com';
-const fields = 'id,media_url,caption,username';
-const limit = 5;
-const url = `${endpoint}/${userId}/media/?fields=${fields}&access_token=${token}&count=${limit}`;
+  const token = process.env.INSTAGRAM_ACCESS_TOKEN;
+  const userId = 4226862094085798;
+  const endpoint = "https://graph.instagram.com";
+  const fields = "id,media_url,caption,username";
+  const limit = 5;
+  const url = `${endpoint}/${userId}/media/?fields=${fields}&access_token=${token}&count=${limit}`;
 
   axios
     .get(url)
@@ -15,19 +14,19 @@ const url = `${endpoint}/${userId}/media/?fields=${fields}&access_token=${token}
       callback(null, {
         statusCode: 200,
         headers: {
-          'content-type': 'application/json',
+          "content-type": "application/json",
         },
         body: JSON.stringify(
-          posts.map(i => ({
+          posts.map((i) => ({
             id: i.id,
             url: i.media_url,
             caption: i.caption,
             username: i.username,
-          })),
+          }))
         ),
-      })
+      });
     })
     .catch((e) => {
-      callback(e)
-    })
-}
+      callback(e);
+    });
+};
